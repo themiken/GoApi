9eb9da44abda81c124cba219b63cfef55325c4f4
Miken <loza110259@siigo.com>
2020-11-17 16:46:31 -0500

    Api de prueba miken

--- main.go
+++ main.go
@@ -2,48 +2,24 @@
 // main.go
 package main
 
-import (
-    "encoding/json"
+import (    
     "fmt"
     "log"
-    "io/ioutil"
     "net/http"
-
     "github.com/gorilla/mux"
 )
 
-type Article struct {
-    Id      string    `json:"Id"`
-    Title   string `json:"Title"`
-    Desc    string `json:"desc"`
-    Content string `json:"content"`
-}
-
-var Articles []Article
-
 func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+    fmt.Fprintf(w, "Miken api works")
+    fmt.Println("Test")
 }
-
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Endpoint Hit: returnAllArticles")
-    json.NewEncoder(w).Encode(Articles)
-}
-
-
 
 func handleRequests() {
     myRouter := mux.NewRouter().StrictSlash(true)
     myRouter.HandleFunc("/", homePage)
-    myRouter.HandleFunc("/articles", returnAllArticles)   
-    log.Fatal(http.ListenAndServe(":10000", myRouter))
+    log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
-    Articles = []Article{
-        Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-    }
     handleRequests()
 }
